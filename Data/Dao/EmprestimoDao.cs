4ea07c9afef84cf10198e66e7e5ed6aac71d1d2e
Blarissa <larissabrasil009@gmail.com>
2023-09-14 11:19:06 -0300

    Feat: adicionando dao para classe emprestimo e leitor

---
+++ Data/Dao/EmprestimoDao.cs
@@ -0,0 +1,54 @@
+﻿using Biblioteca.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Data
+{
+    public class EmprestimoDao
+    {
+        private BibliotecaContexto _contexto;
+
+        public EmprestimoDao(BibliotecaContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Adicionar(Emprestimo emprestimo)
+        {
+            _contexto.Emprestimos.Add(emprestimo);
+            _contexto.SaveChanges();
+        }
+
+        public Emprestimo? Listar(int id)
+        {
+            return ListarTodos()
+                .FirstOrDefault(e => e.Id == id);
+        }
+
+        public IList<Emprestimo> ListarTodos()
+        {
+            return _contexto.Emprestimos
+                .Include(e => e.Livro)
+                .Include(e => e.Leitor)
+                .ToList();
+        }
+
+        public IList<Emprestimo> PesquisarPorLeitor(string pesquisa)
+        {
+            return ListarTodos()
+                .Where(e => e.Leitor.Nome.Contains(pesquisa))
+                .ToList();
+        }
+
+        public IList<Emprestimo> PesquisarPorLivro(string pesquisa)
+        {
+            return ListarTodos()
+                .Where(e => e.Livro.Titulo.Contains(pesquisa))
+                .ToList();
+        }
+    }
+}
---
+++ Data/Dao/LeitorDao.cs
@@ -0,0 +1,44 @@
+﻿using Biblioteca.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Data
+{
+    public class LeitorDao
+    {
+        private BibliotecaContexto _contexto;
+
+        public LeitorDao(BibliotecaContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Adicionar(Leitor leitor)
+        {
+            _contexto.Leitores.Add(leitor);
+            _contexto.SaveChanges();
+        }
+
+        public Leitor? Listar(int id)
+        {
+            return _contexto.Leitores
+                .FirstOrDefault(l => l.Id == id);
+        }
+
+        public IList<Leitor> ListarTodos() 
+        {
+            return _contexto.Leitores.ToList();
+        }
+
+        public IList<Leitor> Pesquisar(string pesquisa) 
+        {
+            return ListarTodos()
+                .Where(l => l.Nome.Contains(pesquisa))
+                .ToList();
+        }
+
+    }
+}
